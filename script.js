b6599e0d2418c4818037f3fbca3dcc88f336870b
Nick Gerleman <nick@nickgerleman.com>
2017-02-20 05:48:16 -0600

    Flesh Out Projects

--- script.js
+++ script.js
@@ -6,11 +6,23 @@ let renderCallbacks = [];
 function main() {
     initNavbar();
     initInfoPane();
+    initProjectCards();
 
     (function render() {
         renderCallbacks.forEach(fn => fn());
         requestAnimationFrame(render);
     })();
+
+    document.addEventListener("wheel", ev => {
+        if (document.querySelector('.expanded-project.active'))
+            ev.preventDefault();
+    });
+
+    document.addEventListener("keydown", ev => {
+        let scrollKeys = [33,34,35,36,37,38,39,40];
+        if (document.querySelector('.expanded-project.active') && scrollKeys.includes(ev.which))
+            ev.preventDefault();
+    })
 }
 
 
@@ -22,10 +34,7 @@ function initNavbar() {
     let contactNav = document.getElementById('contact-info-nav');
     let navbar = document.querySelector('nav');
 
-    // Different browsers have weird behavior here
     onRender(updatePos);
-    document.addEventListener('scroll', updatePos);
-
     function updatePos() {
         let navbarTop = hero.getBoundingClientRect().bottom - navbar.offsetHeight;
 
@@ -46,6 +55,7 @@ function initNavbar() {
 
 function onRender(callback) {
     renderCallbacks.push(callback);
+    document.addEventListener('scroll', callback);
 }
 
 
@@ -89,12 +99,92 @@ function checkActive(link, section) {
 
 
 function initInfoPane() {
-        let infoLinks = document.querySelectorAll('#info-list li');
+    let infoLinks = document.querySelectorAll('#info-list li');
 
-        infoLinks.forEach(link => {
-            link.addEventListener('click', () => {
-                infoLinks.forEach(link => link.classList.remove('active'));
-                link.classList.add('active');
-            });
+    infoLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            infoLinks.forEach(link => link.classList.remove('active'));
+            link.classList.add('active');
         });
-    }
+    });
+}
+
+
+function initProjectCards() {
+
+    let bindings = [
+        ['fluffify-card', 'fluffify'],
+        ['stalkr-card',   'stalkr'],
+        ['taa-card',      'taa'],
+        ['pantri-card',   'pantri'],
+        ['nimbus-card',   'nimbus'],
+        ['beacon-card',   'beacon'],
+        ['alisa-card',    'alisa'],
+        ['socal-card',    'socal']
+    ];
+
+    bindings.forEach(binding => {
+        let card = document.getElementById(binding[0]);
+        let desc = document.getElementById(binding[1]);
+
+        card.addEventListener('click', () => expandCard(card, desc));
+        desc.addEventListener('click', ev => ev.stopPropagation());
+        onRender(() => updateDescTransform(card, desc));
+    });
+
+    let projWrap = document.getElementById('expanded-project-wrap');
+    projWrap.addEventListener('click', collapseCard);
+
+}
+
+
+function expandCard(card, desc) {
+    let projWrap = document.getElementById('expanded-project-wrap');
+
+    projWrap.classList.add('active');
+
+    desc.classList.add('expanding');
+    desc.classList.add('active');
+    desc.style.transform = "scale(1)";
+
+    desc.addEventListener('transitionend', () => desc.classList.remove('expanding'), {once: true});
+}
+
+
+function collapseCard() {
+    let projWrap = document.getElementById('expanded-project-wrap');
+    let desc = document.querySelector('.expanded-project.active');
+
+    projWrap.classList.remove('active');
+
+    desc.classList.add('contracting');
+    desc.classList.remove('active');
+    desc.addEventListener('transitionend', () => desc.classList.remove('contracting'), {once: true});
+}
+
+
+function updateDescTransform(card, desc) {
+    if (desc.classList.contains('active'))
+        return;
+
+    let cardStyle = getComputedStyle(card);
+    let descStyle = getComputedStyle(desc);
+
+    let cardWidth = parseFloat(cardStyle.width);
+    let cardHeight = parseFloat(cardStyle.height);
+
+    // First scale the description so that we have a centered card sized description in the middle
+    let scaleX = cardWidth / parseFloat(descStyle.width);
+    let scaleY = cardHeight / parseFloat(descStyle.height);
+
+    // Do the math to translate after scaling (Because actually changing the position kills perf)
+    let descLeft = 0.5 * (window.innerWidth - cardWidth);
+    let descTop = 0.5 * (window.innerHeight - cardHeight)
+
+    let cardRect = card.getBoundingClientRect();
+    let offsetX = cardRect.left - descLeft;
+    let offsetY = cardRect.top - descTop;
+
+    desc.style.transform = `translate(${offsetX}px, ${offsetY}px) scale(${scaleX}, ${scaleY})`;
+
+}
